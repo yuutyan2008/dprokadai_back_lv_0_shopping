9592dc8e3085844a0c98c555a060811da848e0d6
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2020-12-17 13:40:01 +0900

    Update seller.rb

--- lib/shopping_app/seller.rb
+++ lib/shopping_app/seller.rb
@@ -2,7 +2,7 @@ require_relative "user"
 
 class Seller < User
   def initialize(name)
-    super(name) # TODO: 継承関係のテキストのリンクを添える
+    super(name) # superの役割について確認したい場合は[https://diver.diveintocode.jp/curriculums/2360]のテキストを参考にしてください。
   end
 
 end
