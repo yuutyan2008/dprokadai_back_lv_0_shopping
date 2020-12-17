42cf80ed1f813ffe5f8e66ed7db87afebb5bc581
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2020-12-17 13:41:42 +0900

    Update customer.rb

--- lib/shopping_app/customer.rb
+++ lib/shopping_app/customer.rb
@@ -5,7 +5,7 @@ class Customer < User
   attr_reader :cart
 
   def initialize(name)
-    super(name) # TODO: 継承関係のテキストのリンクを添える
+    super(name) # superの役割について確認したい場合は[https://diver.diveintocode.jp/curriculums/2360]のテキストを参考にしてください。
     @cart = Cart.new(self) # Customerインスタンスは生成されると、自身をオーナーとするカートを持ちます。
   end
 
